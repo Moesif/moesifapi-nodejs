246b90067c5826174f1950c3b13ecc2aa25e7d1e
dgilling <derric@moesif.com>
2017-03-16 17:04:15 -0700

    Fix: Incorrect import of UserModel.
    Fix: Wasn’t importing UserModel into lib.
    Update README with examples for updating users
    Bump version to 1.2.4

--- README.md
+++ README.md
@@ -8,108 +8,268 @@
 __Check out Moesif's [Developer Documentation](https://www.moesif.com/docs) and [Node API Reference](https://www.moesif.com/docs/api?javascript) to learn more__
 
 
-## Install from NPM:
+## How to install
 
 ```shell
 npm install moesifapi
 ```
 
-## How To Use:
+## How to use
 
+(See test/ApiControllerTest.js for more usage examples)
 
-(See ApiControllerTest.js for usage examples)
+### Create a single API event
 
 ```javascript
-///////////////////////////
-// 1. Import the module: //
-///////////////////////////
-    var moesifapi = require('moesifapi');
+// 1. Import the module
+var moesifapi = require('moesifapi');
+var api = moesifapi.ApiController;
 
+// 2. Configure the ApplicationId
+var config = moesifapi.configuration;
+config.ApplicationId = "my_application_id";
 
-////////////////////////////////
-// 2. Configure ApplicationId //
-////////////////////////////////
-    var config = moesifapi.configuration;
-    config.ApplicationId = my_application_id;
+// 3. Generate an API Event Model
+var reqHeaders = JSON.parse('{' +
+        '"Host": "api.acmeinc.com",' +
+        '"Accept": "*/*",' +
+        '"Connection": "Keep-Alive",' +
+        '"User-Agent": "Dalvik/2.1.0 (Linux; U; Android 5.0.2; C6906 Build/14.5.A.0.242)",' +
+        '"Content-Type": "application/json",' +
+        '"Content-Length": "126",' +
+        '"Accept-Encoding": "gzip"' +
+    '}');
 
+var reqBody = JSON.parse( '{' +
+        '"items": [' +
+            '{' +
+                '"type": 1,' +
+                '"id": "fwfrf"' +
+            '},' +
+            '{' +
+                '"type": 2,' +
+                '"id": "d43d3f"' +
+            '}' +
+        ']' +
+    '}');
 
-///////////////////////////////
-// 3. Create API Event Model //
-///////////////////////////////
-    var reqHeaders = JSON.parse('{' +
-            '"Host": "api.acmeinc.com",' +
-            '"Accept": "*/*",' +
-            '"Connection": "Keep-Alive",' +
-            '"User-Agent": "Dalvik/2.1.0 (Linux; U; Android 5.0.2; C6906 Build/14.5.A.0.242)",' +
-            '"Content-Type": "application/json",' +
-            '"Content-Length": "126",' +
-            '"Accept-Encoding": "gzip"' +
-        '}');
+var rspHeaders = JSON.parse('{' +
+        '"Date": "Tue, 23 Jan 2017 23:46:49 GMT",' +
+        '"Vary": "Accept-Encoding",' +
+        '"Pragma": "no-cache",' +
+        '"Expires": "-1",' +
+        '"Content-Type": "application/json; charset=utf-8",' +
+        '"Cache-Control": "no-cache"' +
+    '}');
 
-    var reqBody = JSON.parse( '{' +
-            '"items": [' +
-                '{' +
-                    '"type": 1,' +
-                    '"id": "fwfrf"' +
-                '},' +
-                '{' +
-                    '"type": 2,' +
-                    '"id": "d43d3f"' +
-                '}' +
-            ']' +
-        '}');
+var rspBody = JSON.parse('{' +
+        '"Error": "InvalidArgumentException",' +
+        '"Message": "Missing field field_a"' +
+    '}');
 
-    var rspHeaders = JSON.parse('{' +
-            '"Date": "Tue, 23 Aug 2016 23:46:49 GMT",' +
-            '"Vary": "Accept-Encoding",' +
-            '"Pragma": "no-cache",' +
-            '"Expires": "-1",' +
-            '"Content-Type": "application/json; charset=utf-8",' +
-            '"Cache-Control": "no-cache"' +
-        '}');
+var eventReq = {
+    time: "2017-01-09T04:45:42.914",
+    uri: "https://api.acmeinc.com/items/reviews/",
+    verb: "PATCH",
+    apiVersion: "1.1.0",
+    ipAddress: "61.48.220.123",
+    headers: reqHeaders,
+    body: reqBody
+};
 
-    var rspBody = JSON.parse('{' +
-            '"Error": "InvalidArgumentException",' +
-            '"Message": "Missing field field_a"' +
-        '}');
+var eventRsp = {
+    time: "2017-01-09T04:45:42.914",
+    status: 500,
+    headers: rspHeaders,
+    body: rspBody
+};
 
-    var eventReq = {
-        time: "2016-09-09T04:45:42.914",
-        uri: "https://api.acmeinc.com/items/reviews/",
-        verb: "PATCH",
-        apiVersion: "1.1.0",
-        ipAddress: "61.48.220.123",
-        headers: reqHeaders,
-        body: reqBody
-    };
+var eventModel = {
+    request: eventReq,
+    response: eventRsp,
+    userId: "my_user_id",
+    sessionToken: "23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f"
+};
 
-    var eventRsp = {
-        time: "2016-09-09T04:45:42.914",
-        status: 500,
-        headers: rspHeaders,
-        body: rspBody
-    };
-
-    var eventModel = {
-        request: eventReq,
-        response: eventRsp,
-        userId: "my_user_id",
-        sessionToken: "23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f"
-    };
-
-
-//////////////////////////
-// 4. Create new event: //
-//////////////////////////
-    var controller = moesifapi.ApiController;
-
-    // Create a single event
-    controller.createEvent(new EventModel(eventModel), callback);
-
-    // Create batched set of events
-    controller.createEventsBatch(someEventModelList, callback);
+// 4. Create a single event
+api.createEvent(new EventModel(eventModel), function(error, response, context) {
+  // Do Something
+});
 ```
 
+### Create a batch of API events
+
+You can also create a batch of events at once by sending a list of events.
+
+```javascript
+// 1. Import the module
+var moesifapi = require('moesifapi');
+var api = moesifapi.ApiController;
+
+// 2. Configure the ApplicationId
+var config = moesifapi.configuration;
+config.ApplicationId = "my_application_id";
+
+// 3. Generate an API Event Model
+var reqHeaders = JSON.parse('{' +
+        '"Host": "api.acmeinc.com",' +
+        '"Accept": "*/*",' +
+        '"Connection": "Keep-Alive",' +
+        '"User-Agent": "Dalvik/2.1.0 (Linux; U; Android 5.0.2; C6906 Build/14.5.A.0.242)",' +
+        '"Content-Type": "application/json",' +
+        '"Content-Length": "126",' +
+        '"Accept-Encoding": "gzip"' +
+    '}');
+
+var reqBody = JSON.parse( '{' +
+        '"items": [' +
+            '{' +
+                '"type": 1,' +
+                '"id": "fwfrf"' +
+            '},' +
+            '{' +
+                '"type": 2,' +
+                '"id": "d43d3f"' +
+            '}' +
+        ']' +
+    '}');
+
+var rspHeaders = JSON.parse('{' +
+        '"Date": "Tue, 25 Feb 2017 23:46:49 GMT",' +
+        '"Vary": "Accept-Encoding",' +
+        '"Pragma": "no-cache",' +
+        '"Expires": "-1",' +
+        '"Content-Type": "application/json; charset=utf-8",' +
+        '"Cache-Control": "no-cache"' +
+    '}');
+
+var rspBody = JSON.parse('{' +
+        '"Error": "InvalidArgumentException",' +
+        '"Message": "Missing field field_a"' +
+    '}');
+
+var eventReq = {
+    time: "2017-02-25T04:45:42.914",
+    uri: "https://api.acmeinc.com/items/reviews/",
+    verb: "PATCH",
+    apiVersion: "1.1.0",
+    ipAddress: "61.48.220.123",
+    headers: reqHeaders,
+    body: reqBody
+};
+
+var eventRsp = {
+    time: "2016-09-09T04:45:42.914",
+    status: 500,
+    headers: rspHeaders,
+    body: rspBody
+};
+
+var eventModel = {
+    request: eventReq,
+    response: eventRsp,
+    userId: "my_user_id",
+    sessionToken: "23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f"
+};
+
+var events = [new EventModel(eventModel),
+  new EventModel(eventModel),
+  new EventModel(eventModel),
+  new EventModel(eventModel)];
+
+// 4. Send batch of events
+api.createEventsBatch(events, function(error, response, context) {
+  // Do Something
+});
+```
+
+### Update an end user
+
+Updating an end user will create one if it does not exist,
+also know as [upsert](https://en.wikipedia.org/wiki/Merge_(SQL))
+The only __required__ field is `user_id`.
+
+```javascript
+// 1. Import the module
+var moesifapi = require('moesifapi');
+var api = moesifapi.ApiController;
+
+// 2. Configure the ApplicationId
+var config = moesifapi.configuration;
+config.ApplicationId = "my_application_id";
+
+// 3. Generate a User Model
+var user = {
+    userId: "my_user_id",
+    sessionToken: "23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f",
+    metadata: {
+      email: "johndoe@acmeinc.com",
+      string_field: "value_1",
+      number_field: 0,
+      object_field: {
+        field_a: "value_a",
+        field_b: "value_b"
+      }
+    }
+};
+
+// 4. Create a single user
+api.updateUser(new UserModel(user), function(error, response, context) {
+  // Do Something
+});
+```
+
+### Update a batch of end users
+
+Will update all users in a single batch, useful for saving from offline sources like CSV.
+Any user that does not exist will be created, also known as [upsert](https://en.wikipedia.org/wiki/Merge_(SQL))
+The only __required__ field is `user_id`.
+
+```javascript
+// 1. Import the module
+var moesifapi = require('moesifapi');
+var api = moesifapi.ApiController;
+
+// 2. Configure the ApplicationId
+var config = moesifapi.configuration;
+config.ApplicationId = "my_application_id";
+
+// 3. Generate an API Event Model
+var userA = {
+    userId: "12345",
+    sessionToken: "23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f",
+    metadata: {
+      email: "johndoe@acmeinc.com",
+      string_field: "value_1",
+      number_field: 0,
+      object_field: {
+        field_a: "value_a",
+        field_b: "value_b"
+      }
+    }
+};
+
+var userB = {
+    userId: "6789",
+    sessionToken: "23jdf0oszfexfqe[lwjfiefovprewv4d8ayrcdx8nu2ng]zfeeadedefx43f",
+    metadata: {
+      email: "maryjane@acmeinc.com",
+      string_field: "value_1",
+      number_field: 1,
+      object_field: {
+        field_a: "value_a",
+        field_b: "value_b"
+      }
+    }
+};
+
+var users = [new UserModel(userA), new UserModel(userB)];
+
+// 4. Send batch of events
+api.updateUsersBatch(users, function(error, response, context) {
+  // Do Something
+});
+```
 
 ## How To Test:
 
--- test/ApiControllerTest.js
+++ test/ApiControllerTest.js
@@ -84,8 +84,73 @@ describe('TestAddBatchedEvents', function() {
     it('createEventsBatch() should return 201 HTTP status', function(done) {
         var controller = moesifapi.ApiController;
 
-        var events = "[{ 					\"request\": { 						\"time\": \"2016-02-25T04:45:42.914\", 						\"uri\": \"https://api.acmeinc.com/items/reviews/\", 						\"verb\": \"PATCH\", 						\"api_version\": \"1.1.0\", 						\"ip_address\": \"61.48.220.123\", 						\"headers\": { 							\"Host\": \"api.acmeinc.com\", 							\"Accept\": \"*/*\", 							\"Connection\": \"Keep-Alive\", 							\"User-Agent\": \"Dalvik/2.1.0 (Linux; U; Android 5.0.2; C6906 Build/14.5.A.0.242)\", 							\"Content-Type\": \"application/json\", 							\"Content-Length\": \"126\", 							\"Accept-Encoding\": \"gzip\" 						}, 						\"body\": { 							\"items\": [ 								{ 									\"direction_type\": 1, 									\"discovery_id\": \"fwfrf\", 									\"liked\": false 								}, 								{ 									\"direction_type\": 2, 									\"discovery_id\": \"d43d3f\", 									\"liked\": true 								} 							] 						} 					}, 					\"response\": { 						\"time\": \"2017-02-25T04:45:42.914\", 						\"status\": 500, 						\"headers\": { 							\"Date\": \"Tue, 25 Feb 2017 23:46:49 GMT\", 							\"Vary\": \"Accept-Encoding\", 							\"Pragma\": \"no-cache\", 							\"Expires\": \"-1\", 							\"Content-Type\": \"application/json; charset=utf-8\", 							\"X-Powered-By\": \"ARR/3.0\", 							\"Cache-Control\": \"no-cache\", 							\"Arr-Disable-Session-Affinity\": \"true\" 						}, 						\"body\": { 							\"Error\": \"InvalidArgumentException\", 							\"Message\": \"Missing field field_a\" 						} 					}, 					\"user_id\": \"mndug437f43\", 					\"session_token\": \"23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f\" 					 }, { 					\"request\": { 						\"time\": \"2017-10-09T04:46:42.914\", 						\"uri\": \"https://api.acmeinc.com/items/reviews/\", 						\"verb\": \"PATCH\", 						\"api_version\": \"1.1.0\", 						\"ip_address\": \"61.48.220.123\", 						\"headers\": { 							\"Host\": \"api.acmeinc.com\", 							\"Accept\": \"*/*\", 							\"Connection\": \"Keep-Alive\", 							\"User-Agent\": \"Dalvik/2.1.0 (Linux; U; Android 5.0.2; C6906 Build/14.5.A.0.242)\", 							\"Content-Type\": \"application/json\", 							\"Content-Length\": \"126\", 							\"Accept-Encoding\": \"gzip\" 						}, 						\"body\": { 							\"items\": [ 								{ 									\"direction_type\": 1, 									\"discovery_id\": \"fwfrf\", 									\"liked\": false 								}, 								{ 									\"direction_type\": 2, 									\"discovery_id\": \"d43d3f\", 									\"liked\": true 								} 							] 						} 					}, 					\"response\": { 						\"time\": \"2016-09-09T04:46:42.914\", 						\"status\": 500, 						\"headers\": { 							\"Date\": \"Tue, 23 Aug 2016 23:46:49 GMT\", 							\"Vary\": \"Accept-Encoding\", 							\"Pragma\": \"no-cache\", 							\"Expires\": \"-1\", 							\"Content-Type\": \"application/json; charset=utf-8\", 							\"X-Powered-By\": \"ARR/3.0\", 							\"Cache-Control\": \"no-cache\", 							\"Arr-Disable-Session-Affinity\": \"true\" 						}, 						\"body\": { 							\"Error\": \"InvalidArgumentException\", 							\"Message\": \"Missing field field_a\" 						} 					}, 					\"user_id\": \"mndug437f43\", 					\"session_token\": \"23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f\" 					 }, { 					\"request\": { 						\"time\": \"2016-09-09T04:47:42.914\", 						\"uri\": \"https://api.acmeinc.com/items/reviews/\", 						\"verb\": \"PATCH\", 						\"api_version\": \"1.1.0\", 						\"ip_address\": \"61.48.220.123\", 						\"headers\": { 							\"Host\": \"api.acmeinc.com\", 							\"Accept\": \"*/*\", 							\"Connection\": \"Keep-Alive\", 							\"User-Agent\": \"Dalvik/2.1.0 (Linux; U; Android 5.0.2; C6906 Build/14.5.A.0.242)\", 							\"Content-Type\": \"application/json\", 							\"Content-Length\": \"126\", 							\"Accept-Encoding\": \"gzip\" 						}, 						\"body\": { 							\"items\": [ 								{ 									\"direction_type\": 1, 									\"discovery_id\": \"fwfrf\", 									\"liked\": false 								}, 								{ 									\"direction_type\": 2, 									\"discovery_id\": \"d43d3f\", 									\"liked\": true 								} 							] 						} 					}, 					\"response\": { 						\"time\": \"2016-09-09T04:47:42.914\", 						\"status\": 500, 						\"headers\": { 							\"Date\": \"Tue, 23 Aug 2016 23:46:49 GMT\", 							\"Vary\": \"Accept-Encoding\", 							\"Pragma\": \"no-cache\", 							\"Expires\": \"-1\", 							\"Content-Type\": \"application/json; charset=utf-8\", 							\"X-Powered-By\": \"ARR/3.0\", 							\"Cache-Control\": \"no-cache\", 							\"Arr-Disable-Session-Affinity\": \"true\" 						}, 						\"body\": { 							\"Error\": \"InvalidArgumentException\", 							\"Message\": \"Missing field field_a\" 						} 					}, 					\"user_id\": \"mndug437f43\", 					\"session_token\": \"23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f\" 					 }, { 					\"request\": { 						\"time\": \"2016-09-09T04:48:42.914\", 						\"uri\": \"https://api.acmeinc.com/items/reviews/\", 						\"verb\": \"PATCH\", 						\"api_version\": \"1.1.0\", 						\"ip_address\": \"61.48.220.123\", 						\"headers\": { 							\"Host\": \"api.acmeinc.com\", 							\"Accept\": \"*/*\", 							\"Connection\": \"Keep-Alive\", 							\"User-Agent\": \"Dalvik/2.1.0 (Linux; U; Android 5.0.2; C6906 Build/14.5.A.0.242)\", 							\"Content-Type\": \"application/json\", 							\"Content-Length\": \"126\", 							\"Accept-Encoding\": \"gzip\" 						}, 						\"body\": { 							\"items\": [ 								{ 									\"direction_type\": 1, 									\"discovery_id\": \"fwfrf\", 									\"liked\": false 								}, 								{ 									\"direction_type\": 2, 									\"discovery_id\": \"d43d3f\", 									\"liked\": true 								} 							] 						} 					}, 					\"response\": { 						\"time\": \"2016-09-09T04:48:42.914\", 						\"status\": 500, 						\"headers\": { 							\"Date\": \"Tue, 23 Aug 2016 23:46:49 GMT\", 							\"Vary\": \"Accept-Encoding\", 							\"Pragma\": \"no-cache\", 							\"Expires\": \"-1\", 							\"Content-Type\": \"application/json; charset=utf-8\", 							\"X-Powered-By\": \"ARR/3.0\", 							\"Cache-Control\": \"no-cache\", 							\"Arr-Disable-Session-Affinity\": \"true\" 						}, 						\"body\": { 							\"Error\": \"InvalidArgumentException\", 							\"Message\": \"Missing field field_a\" 						} 					}, 					\"user_id\": \"mndug437f43\", 					\"session_token\": \"exfzweachxjgznvKUYrxFcxv]s98y18cx98q3yhwmnhcfx43f\" 					 }, { 					\"request\": { 						\"time\": \"2016-09-09T04:49:42.914\", 						\"uri\": \"https://api.acmeinc.com/items/reviews/\", 						\"verb\": \"PATCH\", 						\"api_version\": \"1.1.0\", 						\"ip_address\": \"61.48.220.123\", 						\"headers\": { 							\"Host\": \"api.acmeinc.com\", 							\"Accept\": \"*/*\", 							\"Connection\": \"Keep-Alive\", 							\"User-Agent\": \"Dalvik/2.1.0 (Linux; U; Android 5.0.2; C6906 Build/14.5.A.0.242)\", 							\"Content-Type\": \"application/json\", 							\"Content-Length\": \"126\", 							\"Accept-Encoding\": \"gzip\" 						}, 						\"body\": { 							\"items\": [ 								{ 									\"direction_type\": 1, 									\"discovery_id\": \"fwfrf\", 									\"liked\": false 								}, 								{ 									\"direction_type\": 2, 									\"discovery_id\": \"d43d3f\", 									\"liked\": true 								} 							] 						} 					}, 					\"response\": { 						\"time\": \"2016-09-09T04:49:42.914\", 						\"status\": 500, 						\"headers\": { 							\"Date\": \"Tue, 23 Aug 2016 23:46:49 GMT\", 							\"Vary\": \"Accept-Encoding\", 							\"Pragma\": \"no-cache\", 							\"Expires\": \"-1\", 							\"Content-Type\": \"application/json; charset=utf-8\", 							\"X-Powered-By\": \"ARR/3.0\", 							\"Cache-Control\": \"no-cache\", 							\"Arr-Disable-Session-Affinity\": \"true\" 						}, 						\"body\": { 							\"Error\": \"InvalidArgumentException\", 							\"Message\": \"Missing field field_a\" 						} 					}, 					\"user_id\": \"mndug437f43\", 					\"session_token\": \"23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f\" 					 }, { 					\"request\": { 						\"time\": \"2016-09-09T04:50:42.914\", 						\"uri\": \"https://api.acmeinc.com/items/reviews/\", 						\"verb\": \"PATCH\", 						\"api_version\": \"1.1.0\", 						\"ip_address\": \"61.48.220.123\", 						\"headers\": { 							\"Host\": \"api.acmeinc.com\", 							\"Accept\": \"*/*\", 							\"Connection\": \"Keep-Alive\", 							\"User-Agent\": \"Dalvik/2.1.0 (Linux; U; Android 5.0.2; C6906 Build/14.5.A.0.242)\", 							\"Content-Type\": \"application/json\", 							\"Content-Length\": \"126\", 							\"Accept-Encoding\": \"gzip\" 						}, 						\"body\": { 							\"items\": [ 								{ 									\"direction_type\": 1, 									\"discovery_id\": \"fwfrf\", 									\"liked\": false 								}, 								{ 									\"direction_type\": 2, 									\"discovery_id\": \"d43d3f\", 									\"liked\": true 								} 							] 						} 					}, 					\"response\": { 						\"time\": \"2016-09-09T04:50:42.914\", 						\"status\": 500, 						\"headers\": { 							\"Date\": \"Tue, 23 Aug 2016 23:46:49 GMT\", 							\"Vary\": \"Accept-Encoding\", 							\"Pragma\": \"no-cache\", 							\"Expires\": \"-1\", 							\"Content-Type\": \"application/json; charset=utf-8\", 							\"X-Powered-By\": \"ARR/3.0\", 							\"Cache-Control\": \"no-cache\", 							\"Arr-Disable-Session-Affinity\": \"true\" 						}, 						\"body\": { 							\"Error\": \"InvalidArgumentException\", 							\"Message\": \"Missing field field_a\" 						} 					}, 					\"user_id\": \"recvreedfef\", 					\"session_token\": \"xcvkrjmcfghwuignrmcmhxdhaaezse4w]s98y18cx98q3yhwmnhcfx43f\" 					 } ]";
-        var request = controller.createEventsBatch(JSON.parse(events), function(error, response, context) {
+        var reqHeaders = JSON.parse('{' +
+                '"Host": "api.acmeinc.com",' +
+                '"Accept": "*/*",' +
+                '"Connection": "Keep-Alive",' +
+                '"User-Agent": "Dalvik/2.1.0 (Linux; U; Android 5.0.2; C6906 Build/14.5.A.0.242)",' +
+                '"Content-Type": "application/json",' +
+                '"Content-Length": "126",' +
+                '"Accept-Encoding": "gzip"' +
+            '}');
+
+        var reqBody = JSON.parse( '{' +
+                '"items": [' +
+                    '{' +
+                        '"type": 1,' +
+                        '"id": "fwfrf"' +
+                    '},' +
+                    '{' +
+                        '"type": 2,' +
+                        '"id": "d43d3f"' +
+                    '}' +
+                ']' +
+            '}');
+
+        var rspHeaders = JSON.parse('{' +
+                '"Date": "Tue, 25 Feb 2017 23:46:49 GMT",' +
+                '"Vary": "Accept-Encoding",' +
+                '"Pragma": "no-cache",' +
+                '"Expires": "-1",' +
+                '"Content-Type": "application/json; charset=utf-8",' +
+                '"Cache-Control": "no-cache"' +
+            '}');
+
+        var rspBody = JSON.parse('{' +
+                '"Error": "InvalidArgumentException",' +
+                '"Message": "Missing field field_a"' +
+            '}');
+
+        var eventReq = {
+            time: "2017-02-25T04:45:42.914",
+            uri: "https://api.acmeinc.com/items/reviews/",
+            verb: "PATCH",
+            apiVersion: "1.1.0",
+            ipAddress: "61.48.220.123",
+            headers: reqHeaders,
+            body: reqBody
+        };
+
+        var eventRsp = {
+            time: "2016-09-09T04:45:42.914",
+            status: 500,
+            headers: rspHeaders,
+            body: rspBody
+        };
+
+        var eventModel = {
+            request: eventReq,
+            response: eventRsp,
+            userId: "my_user_id",
+            sessionToken: "23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f"
+        };
+
+        var events = [new EventModel(eventModel),
+          new EventModel(eventModel),
+          new EventModel(eventModel),
+          new EventModel(eventModel)];
+
+        var request = controller.createEventsBatch(events, function(error, response, context) {
 
             expect(context.response.statusCode).to.equal(201);
             if (error) done(error);
@@ -97,9 +162,62 @@ describe('TestAddBatchedEvents', function() {
 describe('TestUpdateUser', function() {
   it('updateUser() should be success with 201 status', function(done) {
     var controller = moesifapi.ApiController;
-    var user = '{ "userId": "userfrommoesifApi1", "metadata": { "email": "hello@email.com"} }';
+    var user = {
+        userId: "my_user_id",
+        sessionToken: "23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f",
+        metadata: {
+          email: "johndoe@acmeinc.com",
+          string_field: "value_1",
+          number_field: 0,
+          object_field: {
+            field_a: "value_a",
+            field_b: "value_b"
+          }
+        }
+    };
 
-    var request = controller.updateUser(JSON.parse(user), function(error, response, context) {
+    var request = controller.updateUser(new UserModel(user), function(error, response, context) {
+      expect(context.response.statusCode).to.equal(201);
+      if (error) done(error);
+      else done();
+    });
+  });
+});
+
+describe('TestUpdateUsersBatch', function() {
+  it('updateUsersBatch() should be success with 201 status', function(done) {
+    var controller = moesifapi.ApiController;
+    var userA = {
+        userId: "12345",
+        sessionToken: "23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f",
+        metadata: {
+          email: "johndoe@acmeinc.com",
+          string_field: "value_1",
+          number_field: 0,
+          object_field: {
+            field_a: "value_a",
+            field_b: "value_b"
+          }
+        }
+    };
+
+    var userB = {
+        userId: "6789",
+        sessionToken: "23jdf0oszfexfqe[lwjfiefovprewv4d8ayrcdx8nu2ng]zfeeadedefx43f",
+        metadata: {
+          email: "maryjane@acmeinc.com",
+          string_field: "value_1",
+          number_field: 1,
+          object_field: {
+            field_a: "value_a",
+            field_b: "value_b"
+          }
+        }
+    };
+
+    var users = [new UserModel(userA), new UserModel(userB)];
+
+    var request = controller.updateUsersBatch(users, function(error, response, context) {
       expect(context.response.statusCode).to.equal(201);
       if (error) done(error);
       else done();
